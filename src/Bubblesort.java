ede6b5f4cfa08f73f2beb855a278fe85b40d6861
hp <hp@DESKTOP-R6PG1RM>
2023-01-22 19:44:21 -0800

    Review the coding team

---
+++ src/Bubblesort.java
@@ -0,0 +1,26 @@
+
+import java.util.*;
+import java.util.Arrays;
+public class Bubblesort{
+    public static void main(String[] args){
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Enter the size of the element:");
+        int size=scan.nextInt();
+        int arr[]=new int[size];
+        System.out.println("Enter "+size+" elements"); 
+        for(int i=0;i<size;i++)
+            arr[i]=scan.nextInt();
+            for(int i=0;i<(size-1);i++){
+                for(int j=0;j<size-i-1;j++){
+                    if(arr[j]>arr[j+1]){
+                        int swap=arr[j];
+                        arr[j]=arr[j+1];
+                        arr[j+1]=swap;
+                    }
+                }
+            }
+            System.out.println("Sorted list of numbers are :"+Arrays.toString(arr));
+           
+
+    }
+}
